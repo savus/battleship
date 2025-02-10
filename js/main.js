c972214ffb3dd8e31235b773ac9f18bf793c91be
savus <justin.fuzzygreenstuff@gmail.com>
2025-02-10 05:26:45 -0700

    build message-box and refactor animation styles for modal and message-box

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
 const active = "active";
+
 const loaderContainer = ".loader-container";
 const loader = document.querySelector(loaderContainer);
 
+const messageBoxClass = ".message-box";
+const messageBox = document.querySelector(messageBoxClass);
+
+const loadAnimationDelay = 1200;
+const loadAnimationResolve = 1800;
+
 const setActive = (target, selector) => {
-  const activeSelector = document.querySelector(`${selector}.active`);
-  if (activeSelector !== null) activeSelector.classList.remove("active");
-  target.classList.add("active");
+  const activeSelector = document.querySelector(`${selector}.${active}`);
+  if (activeSelector !== null) activeSelector.classList.remove(active);
+  target.classList.add(active);
 };
 
-const removeActive = (target) => target.classList.remove("active");
+const removeActive = (target) => target.classList.remove(active);
 
 const pause = (ms) =>
   new Promise((resolve) => {
     return setTimeout(resolve, ms);
   });
 
+const displayMessageBox = async () => {
+  await pause(500);
+  setActive(messageBox, messageBoxClass);
+};
+
 const beginLoading = async () => {
-  await pause(2000);
+  await pause(loadAnimationDelay);
   setActive(loader, loaderContainer);
-  await pause(5000);
+  await pause(loadAnimationDelay + loadAnimationResolve);
   removeActive(loader);
+  displayMessageBox();
 };
 
 beginLoading();
