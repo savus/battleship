6269596b54aa8010a6afffa69539db9ffa720fd6
savus <justin.fuzzygreenstuff@gmail.com>
2025-02-16 03:22:51 -0700

    finish styling message-box-controls, build typeWords function to display text in message-box-text using a typewriter animation, and delegate a single click event to register each of the message-box-controls buttons

--- js/main.js
+++ js/main.js
@@ -6,8 +6,40 @@ const loadingScreenDuration = 5000;
 
 const messageBoxClass = ".message-box";
 const messageBox = document.querySelector(messageBoxClass);
+const messageControls = ".message-box-controls";
+const messageBoxControls = document.querySelector(messageControls);
+const messageTextClass = ".message-box-text";
+const messageText = document.querySelector(messageTextClass);
 
-console.log(messageBox);
+messageBoxControls.addEventListener("click", ({ target }) => {
+  if (target.dataset.button) {
+    console.log(target.dataset.button);
+  }
+});
+
+/* buttons */
+
+// const btnYes = "btn-yes";
+// const btnNo = "btn-no";
+// const btnPrompt = "btn-prompt";
+// const btnNext = "btn-next";
+// const btnConfirm = "btn-confirm";
+
+// const yesButton = document.querySelector(btnYes);
+// const noButton = document.querySelector(btnNo);
+// const promptButton = document.querySelector(btnPrompt);
+// const nextButton = document.querySelector(btnNext);
+// const confirmButton = document.querySelector(btnConfirm);
+
+/* 
+<div class="btn btn-prompt">Enter</div>
+<div class="btn btn-prev">Prev</div>
+<div class="btn btn-yes">Yes</div>
+<div class="btn btn-no">No</div>
+<div class="btn btn-next">Next</div>
+<div class="btn btn-confirm">Ok</div>
+*/
+
 const setActive = (target, selector = null) => {
   const selectedElement = document.querySelector(`${selector}.${active}`);
   if (selectedElement !== null) selectedElement.classList.remove(active);
@@ -20,6 +52,16 @@ const pause = (ms) =>
   new Promise((resolve) => {
     return setTimeout(resolve, ms);
   });
+
+const typeWords = async (textField, message, typeSpeed = 50) => {
+  const letters = message.split("");
+  let text = "";
+  for (let i = 0; i < letters.length; i++) {
+    text += letters[i];
+    textField.innerText = text;
+    await pause(typeSpeed);
+  }
+};
 
 const beginLoading = async () => {
   setActive(loader);
@@ -34,10 +76,13 @@ setActive(messageBox);
 
 //DEBUGGING
 let testState = false;
+let testMessage =
+  "This is some text that I'm testing for the purposes of the text displayed to the user in my message box.";
 const testControls = () => {
-  if (!testState) setActive(messageBox);
-  else removeActive(messageBox);
-  testState = !testState;
+  // if (!testState) setActive(messageBox);
+  // else removeActive(messageBox);
+  // testState = !testState;
+  typeWords(messageText, testMessage);
 };
 
 document.addEventListener("keyup", (e) => {
